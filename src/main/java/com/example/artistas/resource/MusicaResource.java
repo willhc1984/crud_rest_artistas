a980e40cd4976430a987d90325c94dd04cffd316
will henrique <willhc1984@gmail.com>
2022-07-14 15:55:40 -0300

    Service/Resource Musica - get/post/put/delete - teste com postman

--- src/main/java/com/example/artistas/resource/MusicaResource.java
+++ src/main/java/com/example/artistas/resource/MusicaResource.java
@@ -3,8 +3,12 @@ package com.example.artistas.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,22 @@ public class MusicaResource {
 	@GetMapping(value = "/{id}")
 	public Musica buscarPorId(@PathVariable Integer id) {
 		Musica musica = service.buscarPorId(id);
-		return musica;		
+		return musica;
 	}
+	
+	@PostMapping
+	public void salvar(@RequestBody Musica musica) {
+		service.salvar(musica);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public void  atualizar(@RequestBody Musica musica) {
+		service.atualizar(musica);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void apagar(@PathVariable Integer id) {
+		service.apagar(id);
+	}
+	
 }
