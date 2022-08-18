49679a3f852cc13405b0e1256c5a9e8454b64ffd
will henrique <willhc1984@gmail.com>
2022-08-18 16:16:22 -0300

    List artistas - get/thymeleaf

--- src/main/java/com/example/artistas/resource/web/ArtistaController.java
+++ src/main/java/com/example/artistas/resource/web/ArtistaController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,12 @@ public class ArtistaController {
 	
 	@Autowired
 	private ArtistaService service;
+	
+	@GetMapping("/lista")
+	public String getAll(ModelMap model) {
+		model.addAttribute("artistas", service.buscarTodos());
+		return "lista-artista";
+	}
 	
 	@GetMapping("/cadastro")
 	public String save(ArtistaDTO artistaDTO) {
